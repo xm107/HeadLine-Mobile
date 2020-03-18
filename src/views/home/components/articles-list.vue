81f4ba8a21d77ec2de9b511ebf1b699c107d4f97
xm107 <1428386916@qq.com>
2020-03-18 16:24:01 +0800

    封装article-list组件

---
+++ src/views/home/components/articles-list.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- 文章列表组件 放置列表 -->
+  <!-- van-list 可以帮我们实现上拉加载  需要变量-->
+  <!-- 这里注意 这个div设置了滚动条 目的是 给后面做 阅读记忆 留下伏笔 -->
+        <!-- 阅读记忆 => 看文章看到一半 滑到中部 去了别的页面 当你回来时 文章还在你看的位置 -->
+  <div class="scroll-wrapper">
+      <!-- 文章列表 -->
+      <!-- van-list组件 如果不加干涉，初始化完毕 就会检测自己距离底部的长度，如果超出了限制，就会执行load事件，自动把绑定的loading 变成true -->
+    <van-list v-model="upLoading" :finished="finished" @load="onLoad">
+    </van-list>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      upLoading: false, // 表示 是否开启了上拉加载 默认值为false
+      finished: false // 表示 是否已经挖成所有数据的加载
+    }
+  },
+  methods: {
+    onLoad () {
+    // 下面这么写 依然不能关掉加载状态 为什么 ? 因为关掉之后  检测机制  高度还是不够 还是会开启
+    //   setTimeout(() => {
+    //     this.upLoading = false
+    //   }, 1000) // 等待一秒 然后关闭加载状态
+      // 如果你有数据 你应该 把数据到加到list中
+      // 如果想关掉
+      setTimeout(() => {
+        this.finished = true // 表示 数据已经全部加载完毕 没有数据了
+      }, 1000) // 等待一秒 然后关闭加载状态
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,13 +5,13 @@
       <!-- 内部放置子标签 -->
       <!-- 内部需要放置 子标签 title 值为当前显示的内容 -->
       <van-tab :title="`标签${item}`" v-for="item in 10" :key="item">
-        <!-- 这里注意 这个div设置了滚动条 目的是 给后面做 阅读记忆 留下伏笔 -->
-        <!-- 阅读记忆 => 看文章看到一半 滑到中部 去了别的页面 当你回来时 文章还在你看的位置 -->
-        <div class="scroll-wrapper">
+        <!-- <div class="scroll-wrapper">
           <van-cell-group>
             <van-cell :title="`标题${item}`" value="内容" v-for="item in 20" :key="item"></van-cell>
           </van-cell-group>
-        </div>
+        </div> -->
+        <!-- 有多少个tab就有多少个articles-list实例 -->
+        <ArticleList></ArticleList>
       </van-tab>
     </van-tabs>
     <span class="bar_btn">
@@ -21,9 +21,12 @@
 </template>
 
 <script>
+import ArticleList from './components/articles-list'
 export default {
   name: 'Home',
-  components: {}
+  components: {
+    ArticleList
+  }
 }
 </script>
 
