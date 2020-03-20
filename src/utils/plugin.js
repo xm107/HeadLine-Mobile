4093f9ed0713b058788cb797e2cbf971f19800dd
xm107 <1428386916@qq.com>
2020-03-20 10:35:51 +0800

    时间格式处理

--- src/utils/plugin.js
+++ src/utils/plugin.js
@@ -1,4 +1,8 @@
-import { Promise } from 'core-js'
+// import { Promise } from 'core-js'
+import dayjs from 'dayjs'// 引入时间戳插件
+import relativeTime from 'dayjs/plugin/relativeTime' // 引入相对时间的扩展
+import 'dayjs/locale/zh-cn' // dayjs不会自动引入语言包 默认支持英文
+dayjs.extend(relativeTime)
 
 /***
  *  plugin.js 专门负责提供小函数以及vue中常用的过滤器及函数
@@ -11,6 +15,9 @@ export default {
     Vue.prototype.$gnotify = (params) => Vue.prototype.$notify({ duration: 800, ...params })
     // 给Vue的原型属性赋值一个函数 自定义一个函数名
     Vue.prototype.$sleep = sleep // 定义一个原型属性$sleep 所有组件都有了这个属性
+    // 过滤器 全局  局部
+    // Vue.filter(名称，函数)
+    Vue.filter('relTime', relTime) // 注册一个全局过滤器
   }
 }
 
@@ -22,4 +29,14 @@ function sleep (time = 500) {
     // 肯定是成功执行 但有一个延迟
     setTimeout(() => resolve(), time)
   })
+}
+
+/**
+ * 次函数用来将日期转化为相对时间
+ * @params date 传入的日期
+ *
+ * **/
+function relTime (date) {
+  // 还需要对dayjs进行语言包转化
+  return dayjs().locale('zh-cn').from(date) // from(中的值是 日期或者dayjs得到的日期) dayjs() 当前时间 dayjs().from(date) 当前时间距离传入的时间有多远
 }
