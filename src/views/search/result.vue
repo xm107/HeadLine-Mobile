e9c7b1e5f78cc56b5db631b3c4075309b8e61cbd
xm107 <1428386916@qq.com>
2020-03-23 15:30:58 +0800

    搜索结果-基础布局

--- src/views/search/result.vue
+++ src/views/search/result.vue
@@ -1,7 +1,34 @@
 <template>
-  <div class="container">
-    <!-- 导航 显示返回箭头 $router.back()与$router.go(-1) 返回上一页-->
-    <van-nav-bar title="搜索结果" left-arrow @click-left="$router.back()"></van-nav-bar>
+  <div class='container'>
+    <!-- 头部导航 显示返回箭头 -->
+    <!-- click-left 点击左侧事件 -->
+    <!-- $router.back()$router.back(-1) -->
+     <van-nav-bar fixed title="搜索结果" left-arrow @click-left="$router.back()" />
+
+      <!-- 放置搜索结果列表 实现上拉加载 -->
+      <van-list>
+        <!-- 页面基本布局 -->
+        <van-cell-group>
+          <van-cell v-for="item in 10" :key="item">
+            <div class="article_item" >
+              <h3 class="van-ellipsis">革命尚未成功，同志仍需努力</h3>
+              <!-- <div class="img_box">
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div> -->
+              <div class="img_box">
+                <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div>
+              <div class="info_box">
+                <span>你像一阵风</span>
+                <span>8评论</span>
+                <span>10分钟前</span>
+              </div>
+            </div>
+          </van-cell>
+        </van-cell-group>
+      </van-list>
   </div>
 </template>
 
@@ -11,6 +38,37 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.container {
+  padding-top: 46px;
+  height: 100%;
+  overflow-y: auto;
+  box-sizing: border-box;
+}
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      height: 180px;
+      width: 100%;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    span {
+      padding-right: 10px;
+    }
+  }
+}
 </style>
