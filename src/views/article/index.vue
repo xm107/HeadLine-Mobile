4c72c5f6f1c9e7ad4b3bd06ce4528f8af6823633
xm107 <1428386916@qq.com>
2020-03-24 12:02:19 +0800

    文章详情-评论组件-评论列表布局

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -21,6 +21,8 @@
         &nbsp;&nbsp;&nbsp;&nbsp;
         <van-button round size="small" :class="{active: article.attitude === 0}"  plain icon="delete">不喜欢</van-button>
       </div>
+        <!-- 放置comment组件 到 detail内部 因为有padding-->
+      <Comment />
     </div>
      <!-- 放置一个遮罩层 -->
     <van-overlay :show="loading" >
@@ -35,7 +37,11 @@
 <script>
 import { getArticleInfo } from '@/api/articles'
 import { followUser, unfollowUser } from '@/api/user'
+import Comment from './components/comment'// 引入评论组件
 export default {
+  comments: {
+    Comment
+  },
   data  () {
     return {
       article: {}, // 接收文章详情数据
@@ -124,6 +130,7 @@ export default {
     position:sticky;
     background-color: #fff;
     top:46px;
+        z-index: 2;
     .text {
       flex: 1;
       padding-left: 10px;
